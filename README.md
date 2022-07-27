9bf58c895002616d1879c320a7fe79c2556cf07f
caoyaping <caoyapingneu@163.com>
2022-07-27 13:41:03 +0800

    save test

---
+++ README.md
@@ -0,0 +1,4 @@
+# PionExample
+
+
+just for test
--- testa/show.go
+++ testa/show.go
@@ -3,3 +3,11 @@ package testa
 func GetA() string {
 	return "GetA"
 }
+
+func GetAA() string {
+	return "GetAA"
+}
+
+func GetAAA() string {
+	return "GetAAA"
+}
